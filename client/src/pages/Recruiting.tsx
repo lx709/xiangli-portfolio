6f77154f1b64288f39897bfa8530f073f9b666cb
lx709 <lixiang709709@gmail.com>
2026-04-11 21:52:46 +0100

    update recruiting

--- client/src/pages/Recruiting.tsx
+++ client/src/pages/Recruiting.tsx
@@ -82,7 +82,7 @@ export default function Recruiting() {
       </nav>
 
       {/* Main Content */}
-      <div className="max-w-4xl mx-auto px-4 py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
 
         {/* Available Positions */}
         <section className="mb-16">
